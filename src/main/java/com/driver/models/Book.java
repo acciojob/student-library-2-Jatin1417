7e7345a65b045993879c7a24ce190a8e84635d0a
Jatin1417 <chaudhari.jatin.18ee1164@gmail.com>
2023-01-27 20:20:29 +0530

    new commit

--- src/main/java/com/driver/models/Book.java
+++ src/main/java/com/driver/models/Book.java
@@ -10,9 +10,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Builder
-@Data
-@AllArgsConstructor
+
 public class Book {
 
     @Id
@@ -43,6 +41,68 @@ public class Book {
     private List<Transaction> transactions;
 
     public Book() {
+    }
+    public Book(String name, Genre genre, Author author) {
+        this.name = name;
+        this.genre = genre;
+        this.author = author;
+        this.available = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
     }
 }
 
